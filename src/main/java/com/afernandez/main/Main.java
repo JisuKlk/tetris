9d24e5b4b1f1621a8badc8746d37ff420e7af7a8
JisuKlk <alvarofdez.f10@gmail.com>
2024-05-31 12:29:48 +0200

    First piece created

--- src/main/java/com/afernandez/main/Main.java
+++ src/main/java/com/afernandez/main/Main.java
@@ -4,11 +4,13 @@ import com.afernandez.utils.Utils;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] table = Utils.initBoard();
-        for(int i = 0; i < table.length; i++) {
-            for(int j = 0; j < table[i].length; j++) {
-                System.out.println(table[i][j]);
+        int[][] form = {{0, 0}, {1, 0}, {1, 0}, {1, 1}};
+        
+        for(int i = 0; i < form.length; i++) {
+            for(int j = 0; j < form[i].length; j++) {
+                System.out.println(form[i][j]);
             }
+            System.out.println();
         }
     }
 }
--- src/main/java/com/afernandez/utils/Pieza.java
+++ src/main/java/com/afernandez/utils/Pieza.java
@@ -1,23 +1,29 @@
 package com.afernandez.utils;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
-public class Pieza {
+public class Pieza {    
 
-    private final String name;
-    private final String color;
-    private final String form;
-    
+    private int[][] form;
 
-    public Pieza(String name, String color, String form) {
-        this.name = name;
-        this.color = color;
+    public Pieza(int[][] form) {
         this.form = form;
     }
 
-    public void createPieza(String form) {
+    public void createPieza() {
+
+        int randomNum = ThreadLocalRandom.current().nextInt(1, 7 + 1);
         
+        switch (randomNum) {
+            case 1:
+            int[][] form = {{0, 0}, {1, 0}, {1, 0}, {1, 1}};
+                Pieza pieza1 = new Pieza(form);
+                System.out.println(pieza1);
+                break;
+        
+            default:
+                break;
+        }
     }
 
 
